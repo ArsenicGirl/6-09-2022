089dae96577033de9f2f7a6e7c3a34d3c37b3506
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2022-09-06 18:04:42 -0500

    Add files via upload

---
+++ 24-08-2022-todo-main/contenidoJS/practicaGuiada/scripts/validate.js
@@ -0,0 +1,10 @@
+const validateString = (cad) =>{
+    //Ternario
+    let response = (cad.length > 3) ? true : false ;
+    return response;
+}
+const validateAverage = (value) =>{
+    let result = (value >= 0 && value <= 5) ? true : false
+    return result;
+}
+export {validateString, validateAverage};
